6e4de56d280eb85c8ae09ca53d410c51c6dd53d7
kiru-N <kselvi28@gmail.com>
2022-02-23 11:44:39 +0800

    Checks for the disk free space percent . If less than 20 exit with 1 else exit with 0 and return everything is ok.

--- disk_usage.py
+++ disk_usage.py
@@ -1,8 +1,17 @@
 import shutil
+import sys
+
 
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
     du_free = du.free / du.total * 100
-    print(round(du_free))
+    return round(du_free)
     
-check_disk_usage("/")
+def main():
+    if check_disk_usage("/") < 20 :
+        print('Not enough space')
+        sys.exit(1)
+    else:
+        print('Everything ok!')
+        sys.exit(0)
+main()
