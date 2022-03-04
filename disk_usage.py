28acd80c8c24dee0245c87bac41722a224ff3d6e
kiru-N <kselvi28@gmail.com>
2022-03-05 06:19:44 +0800

    Adding min_gb and min_percent attributes to check for all possible conditions.

--- disk_usage.py
+++ disk_usage.py
@@ -3,24 +3,23 @@ import shutil
 import sys
 
 
-def check_disk_usage(disk):
+def check_disk_usage(disk,min_gb,min_percent):
     ''' Check for the disk free usage and percentage of disk free.
     Converting the values to GB for the min_gb checks '''
     du = shutil.disk_usage(disk)
     percent_free = du.free / du.total * 100
-
-    if percent_free < 20:
+    free_gb = du.free / 2 ** 30
+    if percent_free < min_percent or free_gb < min_gb:
         return False
     return True
 
 
 def main():
-    if check_disk_usage("/") < 20:
+    if check_disk_usage('/', min_gb=2, min_percent=10):
         print('Not enough space')
         sys.exit(1)
-    else:
-        print('Everything ok!')
-        sys.exit(0)
+    print('Everything ok!')
+    sys.exit(0)
 
 
 main()
