b71a215e82e48f45614816707882c36c4e41c6b0
kiru-N <kselvi28@gmail.com>
2022-03-05 06:12:36 +0800

    Adding comments for all the functions and checks for the disk free percentage less than 20 return True to confirm the space in the disk.

--- disk_usage.py
+++ disk_usage.py
@@ -1,17 +1,26 @@
+''' Importing all the necessary modules for the program '''
 import shutil
 import sys
 
 
 def check_disk_usage(disk):
+    ''' Check for the disk free usage and percentage of disk free.
+    Converting the values to GB for the min_gb checks '''
     du = shutil.disk_usage(disk)
-    du_free = du.free / du.total * 100
-    return round(du_free)
-    
+    percent_free = du.free / du.total * 100
+
+    if percent_free < 20:
+        return False
+    return True
+
+
 def main():
-    if check_disk_usage("/") < 20 :
+    if check_disk_usage("/") < 20:
         print('Not enough space')
         sys.exit(1)
     else:
         print('Everything ok!')
         sys.exit(0)
+
+
 main()
