a18e47cd1edbd82d900503d89f8a2130ed27c5ad
kiru-N <kselvi28@gmail.com>
2022-03-05 06:33:15 +0800

    Adding a new functions to check for any pending reboot. Return True, reboot required.

--- disk_usage.py
+++ disk_usage.py
@@ -1,6 +1,7 @@
 ''' Importing all the necessary modules for the program '''
 import shutil
 import sys
+import os
 
 
 def check_disk_usage(disk,min_gb,min_percent):
@@ -10,14 +11,23 @@ def check_disk_usage(disk,min_gb,min_percent):
     percent_free = du.free / du.total * 100
     free_gb = du.free / 2 ** 30
     if percent_free < min_percent or free_gb < min_gb:
-        return False
-    return True
+        return True
+    return False
+
+
+def check_reboot():
+    ''' Returns True if the computer has a pending Reboot. '''
+    return os.path.exists('/run/reboot-required')
 
 
 def main():
+    if check_reboot():
+        print('pending reboot')
+
     if check_disk_usage('/', min_gb=2, min_percent=10):
         print('Not enough space')
         sys.exit(1)
+
     print('Everything ok!')
     sys.exit(0)
 
